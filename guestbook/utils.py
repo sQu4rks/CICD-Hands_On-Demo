f0fa8a1fd271a9531c507dba4687a04260d75cd4
Marcel Neidinger (mneiding) <mneiding@cisco.com>
2021-09-03 08:34:01 +0200

    Added exception to utils for better unittest

--- guestbook/utils.py
+++ guestbook/utils.py
@@ -3,6 +3,9 @@ import time
 def get_entry(text, author):
     ret = {}
 
+    # Check that text and author are not None
+    if any(e is None for e in [text, author]):
+        raise Exception(f"Property can't be none")
     ret['text'] = text
     ret['author'] = author
     ret['timestamp'] = time.time()
