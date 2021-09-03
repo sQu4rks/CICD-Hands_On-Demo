83fe9de99edb1f5d60b93d24cc02238a56548acb
Marcel Neidinger (mneiding) <mneiding@cisco.com>
2021-09-03 08:35:36 +0200

    added solution for unittest

--- tests/unit/test_entry.py
+++ tests/unit/test_entry.py
@@ -3,4 +3,19 @@ import unittest
 from guestbook.utils import get_entry
 
 class TestEntry(unittest.TestCase):
-    pass
+    def test_succesful_init(self):
+        e = get_entry("text", "author")
+        self.assertIsNotNone(e)
+
+    def test_none_arg_raises_exception(self):
+        self.assertRaises(Exception, get_entry, "text", None)
+        self.assertRaises(Exception, get_entry, None, "author")
+        self.assertRaises(Exception, get_entry, None, None)
+
+    def test_keys_present(self):
+        e = get_entry("text", "author")
+
+        self.assertIn("text", e)
+        self.assertIn("author", e)
+        self.assertIn("timestamp", e)
+        
